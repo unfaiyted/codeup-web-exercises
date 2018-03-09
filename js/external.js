15e69cf477012773a4a63f3a0bdf9b1dc4ebd92f
unfaiyted <unfaiyted@gmail.com>
2018-03-09 14:54:50 -0600

    Added code to produce prompt as well as alert information.

---
+++ js/external.js
@@ -0,0 +1,9 @@
+console.log("Hello, from external JavaScript");
+
+alert("Welcome to my website!");
+
+var userInput = prompt("What is your favorite color?");
+
+alert("Great! " + userInput + " is my favorite color too!");
+
+
