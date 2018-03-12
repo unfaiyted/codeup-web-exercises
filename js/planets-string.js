f26e31c0ac43dcfcdca4d0dc3fa42acd0ac70537
unfaiyted <unfaiyted@gmail.com>
2018-03-12 11:38:33 -0500

    Created code to allow for string to convert to array as well as additional manipulation of data within the array

--- js/planets-string.js
+++ js/planets-string.js
@@ -5,23 +5,49 @@
     var planetsArray;
 
     /**
-     * TODO:
      * Convert planetsString to an array, and save it in a variable named
      * planetsArray.
      * console.log planetsArray to check your work
      */
+    planetsArray = planetsString.split('|');
 
-    // console.log(planetsArray);
+    console.log(planetsArray);
 
     /**
-     * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
-     * BONUS:
+     *  -> Formatting the output to be displayed in a webpage.
+
+     */
+    var multiLinePlanetString = '';
+
+    planetsArray.forEach(function (planet) {
+
+        multiLinePlanetString = multiLinePlanetString + planet + "<br>";
+
+    });
+
+    console.log(multiLinePlanetString);
+
+
+    /**
+     * BONUSSSSSSSSSES
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+    var htmlPlanetString = '<ul>';
+
+    planetsArray.forEach(function (planet) {
+
+        htmlPlanetString += '\n<li>' + planet + '<\\li>';
+
+    });
+
+    htmlPlanetString += "\n<\\ul>";
+
+    console.log(htmlPlanetString);
+
 
 })();
