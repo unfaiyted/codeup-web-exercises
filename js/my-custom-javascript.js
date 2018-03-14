01b942e615db6505f3c9df040ac7c6219c36c54d
unfaiyted <unfaiyted@gmail.com>
2018-03-14 09:31:55 -0500

    updating to select element id

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,6 +2,13 @@
 
 $(document).ready(function() {
 
-    // INSERT JAVASCRIPT CODE SAMPLE HERE
+    alert('Page Fully Loaded');
+
+    // Returns value of title id
+    var contents = $('#title').html();
+    alert(contents);
+
+
+
 
 });
