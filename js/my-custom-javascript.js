de322a17df8ff4178fac1572a4cad27c8da7ef24
unfaiyted <unfaiyted@gmail.com>
2018-03-15 09:01:00 -0500

    combining selectors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,17 +1,15 @@
 "use strict";
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-    $('li').css('font-size', '20px').css('background-color', '#FFFF00');
-    $('h1').css('background-color', '#FFFF00');
-    $('p').css('background-color', '#FFFF00');
-
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css('background-color', '#FFFF00');
 
     $('h1').each(function () {
 
         alert($(this).html());
-   })
+    })
 
 
 });
