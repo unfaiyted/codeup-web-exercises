074ef13821d1baf50aab8ec10d1513051cebb5de
unfaiyted <unfaiyted@gmail.com>
2018-03-15 08:59:15 -0500

    alerts more than one object

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -3,9 +3,15 @@
 $(document).ready(function() {
 
 
-    $('li').css('font-size', '20px');
+    $('li').css('font-size', '20px').css('background-color', '#FFFF00');
+    $('h1').css('background-color', '#FFFF00');
+    $('p').css('background-color', '#FFFF00');
 
 
+    $('h1').each(function () {
+
+        alert($(this).html());
+   })
 
 
 });
