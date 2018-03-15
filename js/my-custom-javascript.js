952a786325e30714f372939ce0636e1c48c1bad3
unfaiyted <unfaiyted@gmail.com>
2018-03-15 09:48:00 -0500

    added listener jquery functionality for click, double click and hover.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -3,6 +3,24 @@
 $(document).ready(function () {
 
 
+    // Change bg color on click
+    $('h1').click(function () {
+        $(this).css('background-color','#FFFF00');
+    });
+
+    // Change font size on double click
+    $('p').dblclick(function () {
+        $(this).css('font-size','18px');
+    });
+
+    // Change text color on hover
+    $('li').hover(function () {
+        $(this).css('color','red');
+
+        }, function () {
+        $(this).css('color','black');
+        }
+    );
 
 
 });
