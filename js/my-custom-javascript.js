ba744f41ba49fef19d69971e848398effdcc0838
unfaiyted <unfaiyted@gmail.com>
2018-03-15 09:38:55 -0500

    removing prev custom stuff

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -3,13 +3,6 @@
 $(document).ready(function () {
 
 
-    $('li').css('font-size', '20px');
-    $('h1, p, li').css('background-color', '#FFFF00');
-
-    $('h1').each(function () {
-
-        alert($(this).html());
-    })
 
 
 });
